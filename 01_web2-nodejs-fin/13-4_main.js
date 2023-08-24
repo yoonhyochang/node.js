ed92867c0574621249d559fd66dfec8eda07134c
gptus <yhc9308@naver.com>
2023-08-24 22:56:21 +0900

    App-파일을 읽어서 본문 구현

---
+++ 01_web2-nodejs-fin/13-4_main.js
@@ -0,0 +1,41 @@
+//* 2. 쿼리 스트링의 id 값에 따라 본문 내용 읽어오기
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request, response) {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    var title = queryData.id;
+    if(_url == '/') {
+        title = 'Welcome';
+    }
+    if(_url == '/favicon.ico') {
+        return response.writeHead(404);
+    }
+    response.writeHead(200);
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) { // 여기
+        var template = `
+        <!doctype html>
+        <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${title}</h2>
+                <p>${description}</p> //여기
+            </body>
+        </html>
+        `;
+        response.end(template);
+    });
+});
+app.listen(3000);
