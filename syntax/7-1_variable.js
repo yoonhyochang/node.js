63a8a2251a3cdcabee3ec1c4d08786c4ad1b5f95
gptus <yhc9308@naver.com>
2023-08-24 17:23:48 +0900

    자바스크립트-변수

---
+++ syntax/7-1_variable.js
@@ -0,0 +1,13 @@
+//* 변수 사용법
+
+a = 1;	// ← a 변수 선언 후 숫자 1 대입
+console.log(a);
+a = 2;	// ← a 변수에 숫자 2 대입
+console.log(a);
+// 1 = 2;	// ← 오류 발생!
+
+//* var 키워드 변수선언
+var a = 1;	// ← a 변수 선언 후 숫자 1 대입
+console.log(a);
+a = 2;      // ← a 변수에 숫자 2 대입
+console.log(a);
---
+++ syntax/7-6_variable2.js
@@ -0,0 +1,8 @@
+//* 변수의 활용
+var letter = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
+
+
+var name = 'egoing';
+var letter2 = 'Dear ' + name + 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' + name + ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. ' + name;
+console.log(letter2);
