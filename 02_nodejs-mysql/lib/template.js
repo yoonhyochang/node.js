8c2fac800b3822096bd976421763211bb79e949d
gptus <yhc9308@naver.com>
2023-08-28 20:52:39 +0900

    템플릿 업데이트(무시)

--- 02_nodejs-mysql/lib/template.js
+++ 02_nodejs-mysql/lib/template.js
@@ -1,4 +1,4 @@
-//* 3. 작성자를 출력하는 콤보 박스를 템플릿으로 분리
+//* 3. authorSelect 함수에 두 번째 인수를 처리하는 코드 추가
 module.exports = {
     HTML:function(title, list, body, control) {
         return `
@@ -25,11 +25,15 @@ module.exports = {
         }
         list = list+'</ul>';
         return list;
-    },authorSelect:function(authors) {//여기부터
+    },authorSelect:function(authors, author_id) { //여기
         var tag = '';
         var i = 0;
-        while(i < authors.length) {
-            tag += `<option value="${authors[i].id}">${authors[i].name}</option>`;
+        while(i < authors.length) {//여기부터
+            var selected = '';
+            if(authors[i].id === author_id) {
+                selected = ' selected';
+            }//여기까지
+            tag += `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;//여기
             i++;
         }
         return `
@@ -37,5 +41,5 @@ module.exports = {
                 ${tag}
             </select>
         `
-    } //여기까지
+    }
 }
